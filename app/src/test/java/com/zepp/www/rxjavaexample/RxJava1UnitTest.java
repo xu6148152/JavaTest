f958aeb24a1b6976572337b2130fb73110ab0d0a
xubinggui <xubinggui@zepplabs.com>
2017-01-01 18:15:41 +0800

    Add UnitTest

--- app/src/test/java/com/zepp/www/rxjavaexample/RxJava1UnitTest.java
+++ app/src/test/java/com/zepp/www/rxjavaexample/RxJava1UnitTest.java
@@ -24,22 +24,49 @@ package com.zepp.www.rxjavaexample;
 //                  佛祖镇楼                  BUG辟易 
 
 import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.observables.BlockingObservable;
+import rx.observables.ConnectableObservable;
+import rx.observables.MathObservable;
 import rx.observables.SyncOnSubscribe;
+import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 @RunWith(MockitoJUnitRunner.class) public class RxJava1UnitTest {
+    static Integer[] integers = new Integer[9];
+    static Integer[] integers1 = new Integer[9];
+
+    static {
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = i;
+            integers1[i] = -i;
+        }
+    }
+
     @Mock Context mContext;
+
+    @Before public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test public void testCreateObservable() {
         Observable.create(new SyncOnSubscribe<Object, Object>() {
@@ -87,23 +114,348 @@ import rx.schedulers.Schedulers;
     }
 
     @Test public void testDeferObservable() {
+        //SomeType someType  = new SomeType();
+        //someType.setValue("Some Value");
+        //someType.valueObservable().subscribe(System.out::println);
         Observable.defer(Observable::empty).subscribe(new Subscriber<Object>() {
             @Override public void onCompleted() {
-
+                System.out.println("onCompleted");
             }
 
             @Override public void onError(Throwable e) {
-
+                System.out.println("onError");
             }
 
             @Override public void onNext(Object o) {
-
+                System.out.println("onNext");
             }
         });
     }
 
+    @Test public void testFromFuture() {
+        Observable.from(Executors.newSingleThreadExecutor().submit(() -> {
+            System.out.println("before sleep");
+            Thread.sleep(5000);
+            return true;
+        })).subscribe(mSubscriber);
+        mSubscriber.unsubscribe();
+    }
+
+    @Test public void testTimeInterval() {
+        Observable.from(integers).timeInterval().subscribe(mSubscriber);
+    }
+
+    @Test public void testTimeOut() {
+        Observable.from(integers).timeout(integer -> Observable.from(integers1)).subscribe(mSubscriber);
+    }
+
+    @Test public void testTimeStamp() {
+        Observable.from(integers).timestamp().subscribe(mSubscriber);
+    }
+
+    @Test public void testAll() {
+        Observable.from(integers).all(integer -> integer < 5).subscribe(mSubscriber);
+    }
+
+    @Test public void testContain() {
+        Observable.from(integers).contains(1).subscribe(mSubscriber);
+    }
+
+    @Test public void testDefaultEmpty() {
+        Observable.empty().defaultIfEmpty(11).subscribe(mSubscriber);
+    }
+
+    @Test public void testSequenceEqual() {
+        Observable.sequenceEqual(Observable.from(integers), Observable.from(integers)).subscribe(mSubscriber);
+    }
+
+    @Test public void testSkipUtil() {
+        Observable.from(integers).skipUntil(Observable.from(integers1).delay(1, TimeUnit.SECONDS)).subscribe(mSubscriber);
+    }
+
+    @Test public void testSkipWhile() {
+        Observable.from(integers).skipWhile(integer -> integer >= 0).subscribe(mSubscriber);
+    }
+
+    @Test public void testAverage() {
+        MathObservable.from(Observable.from(integers)).averageInteger(integer -> integer).forEach(System.out::println);
+    }
+
+    @Test public void testConcat() {
+        Observable.concat(Observable.from(integers), Observable.from(integers1)).forEach(System.out::println);
+    }
+
+    @Test public void testCount() {
+        assertEquals(Integer.valueOf(integers.length), Observable.from(integers).count().toBlocking().single());
+    }
+
+    @Test public void testMax() {
+        assertEquals(integers[integers.length - 1], MathObservable.from(Observable.from(integers)).max((o1, o2) -> o1 - o2).toBlocking().single());
+    }
+
+    @Test public void testReduce() {
+        Observable.from(integers).reduce((integer, integer2) -> integer + integer2).forEach(System.out::println);
+    }
+
+    @Test public void testConnect() {
+        final AtomicInteger run = new AtomicInteger();
+
+        final ConnectableObservable<Integer> co = Observable.defer(() -> Observable.just(run.incrementAndGet())).publish();
+
+        final Observable<Integer> source = co.autoConnect();
+
+        assertEquals(0, run.get());
+
+        TestSubscriber<Integer> ts1 = TestSubscriber.create();
+        source.subscribe(ts1);
+
+        ts1.assertCompleted();
+        ts1.assertNoErrors();
+        ts1.assertValue(1);
+
+        assertEquals(1, run.get());
+
+        TestSubscriber<Integer> ts2 = TestSubscriber.create();
+        source.subscribe(ts2);
+
+        ts2.assertNotCompleted();
+        ts2.assertNoErrors();
+        ts2.assertNoValues();
+
+        assertEquals(1, run.get());
+    }
+
+    @Test public void testPublish() throws InterruptedException {
+        final AtomicInteger counter = new AtomicInteger();
+        ConnectableObservable<String> o = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override public void call(final Subscriber<? super String> observer) {
+                new Thread(() -> {
+                    counter.incrementAndGet();
+                    observer.onNext("one");
+                    observer.onCompleted();
+                }).start();
+            }
+        }).publish();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+        o.subscribe(v -> {
+            assertEquals("one", v);
+            countDownLatch.countDown();
+        });
+
+        // subscribe again
+        o.subscribe(v -> {
+            assertEquals("one", v);
+            countDownLatch.countDown();
+        });
+
+        final Subscription subscription = o.connect();
+        try {
+            if (!countDownLatch.await(1000, TimeUnit.MILLISECONDS)) {
+                fail("subscriptions did not receive values");
+            }
+            assertEquals(1, counter.get());
+        } finally {
+            subscription.unsubscribe();
+        }
+    }
+
+    @Test public void testRefCount() {
+        final AtomicInteger subscribeCount = new AtomicInteger();
+        final AtomicInteger nextCount = new AtomicInteger();
+        final Observable<Integer> observable = Observable.from(integers)
+                                                         .doOnSubscribe(subscribeCount::incrementAndGet)
+                                                         .doOnNext(l -> nextCount.incrementAndGet())
+                                                         .publish()
+                                                         .refCount();
+        final AtomicInteger receivedCount = new AtomicInteger();
+        final Subscription s1 = observable.subscribe(l -> receivedCount.incrementAndGet());
+        final Subscription s2 = observable.subscribe();
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+        }
+        s2.unsubscribe();
+        s1.unsubscribe();
+
+        System.out.println("onNext Count: " + nextCount.get());
+
+        assertEquals(nextCount.get(), receivedCount.get() * 2);
+
+        assertEquals(2, subscribeCount.get());
+    }
+
+    @Test public void testReplay() throws InterruptedException {
+        final AtomicInteger counter = new AtomicInteger();
+        Observable<String> o = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override public void call(final Subscriber<? super String> observer) {
+                new Thread(() -> {
+                    counter.incrementAndGet();
+                    System.out.println("published observable being executed");
+                    observer.onNext("one");
+                    observer.onCompleted();
+                }).start();
+            }
+        }).replay().autoConnect();
+
+        // we then expect the following 2 subscriptions to get that same value
+        final CountDownLatch latch = new CountDownLatch(2);
+
+        // subscribe once
+        o.subscribe(v -> {
+            assertEquals("one", v);
+            System.out.println("v: " + v);
+            latch.countDown();
+        });
+
+        // subscribe again
+        o.subscribe(v -> {
+            assertEquals("one", v);
+            System.out.println("v: " + v);
+            latch.countDown();
+        });
+
+        if (!latch.await(1000, TimeUnit.MILLISECONDS)) {
+            fail("subscriptions did not receive values");
+        }
+        assertEquals(1, counter.get());
+    }
+
+    @Test public void testAmb() {
+        Observable.amb(Observable.from(integers).delay(2, TimeUnit.SECONDS), Observable.from(integers1).delay(1, TimeUnit.SECONDS))
+                  .toBlocking()
+                  .forEach(System.out::println);
+    }
+
+    @Test public void testUsing() {
+        Observable.using(() -> null, o -> null, o -> {
+
+        }).subscribe(mSubscriber);
+    }
+
+    @Test public void testRepeat() {
+        Observable.range(0, 3).repeat(3).subscribe(mSubscriber);
+    }
+
+    @Test public void testBuffer() {
+        //Observable.from(integers).buffer(2).subscribe(mSubscriber);
+        //System.out.println("buffer count");
+        //Observable.from(integers).buffer(() -> null).subscribe(mSubscriber);
+        //System.out.println("buffer Func0");
+        //Observable.from(integers).buffer(Observable.from(integers)).subscribe(mSubscriber);
+        //System.out.println("buffer Observable");
+        Observable.from(integers).buffer(1, 3, TimeUnit.SECONDS).subscribe(mSubscriber);
+        System.out.println("buffer timespan");
+    }
+
+    @Test public void testWindow() {
+        Observable.from(integers).window(2).subscribe(mSubscriber);
+    }
+
+    @Test public void testDebounce() {
+        Observable.from(integers).debounce(10, TimeUnit.SECONDS).subscribe(mSubscriber);
+        //Observable.from(integers).debounce(Observable::just).subscribe(mSubscriber);
+        //Observable.from(integers).map(integer -> {
+        //    try {
+        //        Thread.sleep(1000);
+        //    } catch (InterruptedException e) {
+        //        e.printStackTrace();
+        //    }
+        //    return integer;
+        //}).debounce(1, TimeUnit.SECONDS).subscribe(mSubscriber);
+    }
+
+    @Test public void testElementAt() {
+        Observable.from(integers).elementAt(5).subscribe(mSubscriber);
+    }
+
+    @Test public void testFirst() {
+        Observable.from(integers).first(integer -> false).subscribe(mSubscriber);
+    }
+
+    @Test public void testIgnoreElements() {
+        Observable.from(integers).ignoreElements().subscribe(mSubscriber);
+    }
+
+    @Test public void testLast() {
+        Observable.from(integers).last().subscribe(mSubscriber);
+    }
+
+    @Test public void testSample() {
+        Observable.from(integers).sample(1, TimeUnit.SECONDS).subscribe(mSubscriber);
+    }
+
+    @Test public void testSkip() {
+        Observable.from(integers).skip(5).subscribe(mSubscriber);
+    }
+
+    @Test public void testSkipLast() {
+        Observable.from(integers).skipLast(5).subscribe(mSubscriber);
+    }
+
+    @Test public void testAndThenWhen() {
+        Observable one = Observable.interval(1, TimeUnit.SECONDS).take(5);
+        Observable two = Observable.interval(250, TimeUnit.MILLISECONDS).take(10);
+        Observable three = Observable.interval(150, TimeUnit.MILLISECONDS).take(14);
+        //final Pattern3 pattern3 = JoinObservable.from(one).and(two).and(three);
+        //JoinObservable.when(pattern3.then((first, second, third) -> first)).toObservable().subscribe(mSubscriber);
+    }
+
+    @Test public void testCombineLatest() {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        MySubscriber subscriber = new MySubscriber();
+        subscriber.setCountDownLatch(countDownLatch);
+        Observable.combineLatest(Observable.interval(1, TimeUnit.SECONDS).take(5), Observable.from(integers), (first, second) -> first + second)
+                  .subscribe(subscriber);
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test public void testJoin() {
+        List<String[]> leftList = new ArrayList<>();
+        leftList.add(new String[] { "2013-01-01 02:00:00", "Batch1" });
+        leftList.add(new String[] { "2013-01-01 03:00:00", "Batch2" });
+        leftList.add(new String[] { "2013-01-01 04:00:00", "Batch3" });
+
+        List<String[]> rightList = new ArrayList<>();
+        rightList.add(new String[] { "2013-01-01 01:00:00", "Production=2" });
+        rightList.add(new String[] { "2013-01-01 02:00:00", "Production=0" });
+        rightList.add(new String[] { "2013-01-01 03:00:00", "Production=3" });
+
+        Observable.from(leftList)
+                  .groupJoin(Observable.from(rightList), a -> Observable.never(), b -> Observable.never(),
+                             (left, obsOfRight) -> Observable.from(left))
+                  .subscribe(mSubscriber);
+    }
+
+    @Test public void testMerge() {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        MySubscriber subscriber = new MySubscriber();
+        subscriber.setCountDownLatch(countDownLatch);
+        Observable.merge(Observable.from(integers), Observable.interval(1, TimeUnit.SECONDS).take(5)).subscribe(subscriber);
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test public void testStartWith() {
+        Observable.from(integers).startWith(100).subscribe(mSubscriber);
+    }
+
+    @Test public void testSwitch() {
+        //Observable.from(integers).switchIfEmpty(Observable.from(integers1)).subscribe(mSubscriber);
+        Observable.from(integers).switchMap(Observable::just).subscribe(mSubscriber);
+    }
+
     @Test public void testLoadThreeDocumentsInParallel() {
-        Observable.just(1, 2, 3).subscribeOn(Schedulers.io()).observeOn(Schedulers.io()).subscribe(new Subscriber<Integer>() {
+        Observable.just(1, 2, 3).subscribe(new Subscriber<Integer>() {
 
             @Override public void onError(Throwable e) {
                 System.err.println("Whoops: " + e.getMessage());
@@ -162,9 +514,10 @@ import rx.schedulers.Schedulers;
 
     @Test public void testCountDownLatch() {
         final CountDownLatch latch = new CountDownLatch(5);
+        long startTime = System.currentTimeMillis();
         Observable.interval(1, TimeUnit.SECONDS).subscribe(counter -> {
             latch.countDown();
-            System.out.println("Got: " + counter);
+            System.out.println("Got: " + counter + " time: " + (System.currentTimeMillis() - startTime));
         });
         try {
             latch.await();
@@ -206,6 +559,60 @@ import rx.schedulers.Schedulers;
         Observable<Integer> evens = Observable.just(2, 4, 6, 8, 10);
         Observable<Integer> odds = Observable.just(1, 3, 5, 7, 9);
         Observable.zip(evens, odds, (v1, v2) -> v1 + " + " + v2 + " is: " + (v1 + v2)).subscribe(System.out::println);
+    }
+
+    @Test public void testCatch() {
+        Observable.from(integers).map(integer -> integer / 0).onErrorReturn(throwable -> 0).subscribe(mSubscriber);
+        Observable.from(integers).map(integer -> integer / 0).onErrorResumeNext(throwable -> Observable.from(integers1)).subscribe(mSubscriber);
+        Observable.from(integers).map(integer -> integer / 0).onExceptionResumeNext(Observable.from(integers1)).subscribe(mSubscriber);
+    }
+
+    @Test public void testRetry2() {
+        Observable.from(integers).map(integer -> {
+            int a = 1;
+            if (integer == a) {
+                throw new RuntimeException("aa");
+            }
+            return integer;
+        }).retry((integer, throwable) -> false).subscribe(mSubscriber);
+    }
+
+    @Test public void testRetryWhen() {
+        Observable.from(integers).map(integer -> {
+            int a = 5;
+            if (integer == a) {
+                throw new RuntimeException("aa");
+            }
+            return integer;
+        }).retryWhen(observable -> observable.zipWith(Observable.range(1, 3), (n, i) -> i).flatMap(i -> {
+            System.out.println("delay retry by " + i + " second(s)");
+            return Observable.timer(i, TimeUnit.SECONDS);
+        })).toBlocking().forEach(System.out::println);
+
+        //Observable.from(integers).map(integer -> {
+        //    int a = 5;
+        //    if (integer == a) {
+        //        throw new RuntimeException("aa");
+        //    }
+        //    return integer;
+        //}).retryWhen(observable -> observable.flatMap((Func1<Throwable, Observable<?>>) throwable -> Observable.from(integers1))).subscribe(mSubscriber);
+    }
+
+    @Test public void testDelay() {
+        //Observable.from(integers).delay(5, TimeUnit.SECONDS).toBlocking().forEach(System.out::println);
+        Observable.from(integers).delaySubscription(5, TimeUnit.SECONDS).toBlocking().forEach(System.out::println);
+    }
+
+    @Test public void testDo() {
+        //Observable.from(integers).doOnEach(notification -> {
+        //    System.out.println(notification.getKind().name());
+        //}).subscribe(mSubscriber);
+
+        Observable.from(integers).doOnRequest(System.out::print).subscribe(mSubscriber);
+    }
+
+    @Test public void testMaterialize() {
+        Observable.from(integers).materialize().dematerialize().subscribe(mSubscriber);
     }
 
     @Test public void testError() {
@@ -262,4 +669,43 @@ import rx.schedulers.Schedulers;
     @Test public void testSubjects() {
 
     }
+
+    class MySubscriber extends Subscriber<Object> {
+        private CountDownLatch mCountDownLatch;
+
+        public void setCountDownLatch(CountDownLatch countDownLatch) {
+            mCountDownLatch = countDownLatch;
+        }
+
+        @Override public void onCompleted() {
+            if (mCountDownLatch != null) {
+                mCountDownLatch.countDown();
+            }
+            System.out.println("onCompleted");
+        }
+
+        @Override public void onError(Throwable e) {
+            System.out.println("onError");
+        }
+
+        @Override public void onNext(Object o) {
+            System.out.println("onNext " + "value: " + o);
+        }
+    }
+
+    private Subscriber<Object> mSubscriber = new Subscriber<Object>() {
+
+        @Override public void onCompleted() {
+
+            System.out.println("onCompleted");
+        }
+
+        @Override public void onError(Throwable e) {
+            System.out.println("onError");
+        }
+
+        @Override public void onNext(Object o) {
+            System.out.println("onNext " + "value: " + o);
+        }
+    };
 }
---
+++ app/src/test/java/com/zepp/www/rxjavaexample/SomeType.java
@@ -0,0 +1,38 @@
+package com.zepp.www.rxjavaexample;
+
+//  Created by xubinggui on 28/12/2016.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+
+import io.reactivex.Observable;
+
+public class SomeType {
+    private String value;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Observable<String> valueObservable() {
+        return Observable.just(value);
+    }
+}
